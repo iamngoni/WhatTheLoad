d93f782d20dc07b16ea87ae6602e054c795ae314
Ngonidzashe Mangudya <imngonii@gmail.com>
2026-02-13 02:47:13 +0200

    Initial Commit

---
+++ WhatTheLoad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatTheLoad
+//
+//  Created by Ngonidzashe  Mangudya on 2026/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatTheLoad/WhatTheLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheLoadApp.swift
+//  WhatTheLoad
+//
+//  Created by Ngonidzashe  Mangudya on 2026/02/13.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
